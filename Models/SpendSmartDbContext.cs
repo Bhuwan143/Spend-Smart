276bc395352a8ed8d4beb2280245b2dfb518a285
Bhuwan Bhatt <167435897+Bhuwan143@users.noreply.github.com>
2025-11-10 22:10:22 +0530

    Add files via upload

---
+++ Models/SpendSmartDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+
+namespace SpendSmart.Models
+{
+    public class SpendSmartDbContext : DbContext
+    {
+       public DbSet<Expense> Expenses { get; set; }
+
+        public SpendSmartDbContext(DbContextOptions <SpendSmartDbContext> options)
+           : base(options)
+        {
+                
+        }
+    }
+}
